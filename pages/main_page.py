68f7be15413facfe3d59ed73096f5887b0547ec9
Арсений Анохин <a.anokhin@IAT>
2022-07-13 13:59:06 +0300

    Наследование и проверки

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,26 +1,17 @@
 from .base_page import BasePage
+from .login_page import LoginPage
 from .locators import MainPageLocators
-from selenium.webdriver.common.by import By
 
-class MainPage(BasePage):
-    def go_to_login_page(self):
-        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        login_link.click() 
+class MainPage(BasePage): 
 
-    def should_be_login_link(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+	def __init__(self, *args, **kwargs):
+		super(MainPage, self).__init__(*args, **kwargs)
 
-    def should_be_login_link(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+# Переносим в BasePage
+#	def go_to_login_page(self):
+#		link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+#		link.click()
+#		return LoginPage(browser=self.browser, url=self.browser.current_url) 
 
-    def go_to_buy_page(self):
-        login_link = self.browser.find_element(*MainPageLocators.bascet_form)
-        login_link.click() 
-
-    def check_item_name(self, name):
-        item_name = self.browser.find_element(By.XPATH, "//article/div[1]/div[2]/h1").text
-        assert item_name == name, "Name is wrong"
-
-    def check_item_price(self, price):
-        item_price = self.browser.find_element(By.XPATH, "//article/div[1]/div[2]/p[1]").text
-        assert item_price == price, "Price is wrong"
+#	def should_be_login_link(self):
+#		assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
