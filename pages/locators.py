c7dcf6d8ec534bff1f132c6a36b65119cf4a72ea
Арсений Анохин <a.anokhin@IAT>
2022-07-13 13:59:51 +0300

    Наследование и проверки

--- pages/locators.py
+++ pages/locators.py
@@ -1,13 +1,31 @@
 from selenium.webdriver.common.by import By
-from .base_page import BasePage
 
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+class MainPageLocators(object):
+	LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
-    bascet_form = (By.CSS_SELECTOR, "#add_to_basket_form")
+class LoginPageLocators(object):
+	LOGIN_FORM		= (By.CSS_SELECTOR, "#login_form")
+	REGISTER_FORM	= (By.CSS_SELECTOR, "#register_form")
+	REG_EMAIL		= (By.CSS_SELECTOR, "#id_registration-email")
+	REG_PASSWORD	= (By.CSS_SELECTOR, "#id_registration-password1")
+	REG_CPASSWORD	= (By.CSS_SELECTOR, "#id_registration-password2")
+	REGISTER_BTN	= (By.CSS_SELECTOR, "[name='registration_submit']")
 
-class LoginPageLocators():
-    login_form = (By.CSS_SELECTOR, "#login_form")
+class ProductPageLocators(object):
+	ADD_BTN				= (By.CSS_SELECTOR, "button.btn-add-to-basket")
+	PRODUCT_NAME		= (By.CSS_SELECTOR, ".product_main h1")
+	ADD_PRODUCT_NAME	= (By.XPATH, "//*[@id='messages']/div[1]/div/strong")
+	PRODUCT_PRICE		= (By.CSS_SELECTOR, ".product_main p")
+	SUM_BASKET			= (By.CSS_SELECTOR, ".basket-mini.pull-right.hidden-xs")
+	SUCCESS_MESSAGE		= (By.XPATH, "//*[@id='messages']/div[1]/div/strong")
 
-    register_form = (By.CSS_SELECTOR, "#register_form")
+class BasePageLocators(object):
+	LOGIN_LINK			= (By.CSS_SELECTOR, "#login_link")
+	LOGIN_LINK_INVALID	= (By.CSS_SELECTOR, "#login_link_inc")
+	CART_LINK			= (By.CSS_SELECTOR, ".btn-default[href*='basket']")
+	CART_LINK_INVALID	= (By.CSS_SELECTOR, "#basket_unlink")
+	USER_ICON			= (By.CSS_SELECTOR, ".icon-user")
 
+class CartPageLocators(object):
+	CART_EMPTY_TXT	= (By.CSS_SELECTOR, "#content_inner p")
+	CART_TOTALS		= (By.CSS_SELECTOR, "#basket_totals")
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,20 +1,27 @@
 from .base_page import BasePage
-from selenium.webdriver.common.by import By
 from .locators import LoginPageLocators
-from .base_page import BasePage
 
 class LoginPage(BasePage):
-    def should_be_login_page(self):
-        self.should_be_login_url()
-        self.should_be_login_form()
-        self.should_be_register_form()
+	def should_be_login_page(self):
+		self.should_be_login_url()
+		self.should_be_login_form()
+		self.should_be_register_form()
 
+	def should_be_login_url(self):
+		# проверка на корректный url адрес
+		assert self.url.find("login") > 0, "URL is not corrected"
 
-    def should_be_login_url(self):
-        assert "login" in self.browser.current_url, "Login url is not presented"
+	def should_be_login_form(self):
+		# проверка, что есть форма логина
+		assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
-    def should_be_login_form(self):
-        assert self.is_element_present(*LoginPageLocators.login_form), "Login form is not presented"
+	def should_be_register_form(self):
+		# проверка, что есть форма регистрации на странице
+		assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
 
-    def should_be_register_form(self):
-        assert self.is_element_present(*LoginPageLocators.register_form), "Register form is not presented"
+	def register_new_user(self, email, password):
+		self.go_to_login_page()
+		assert self.send_keys_element(*LoginPageLocators.REG_EMAIL, email), "Reg email is not exist"
+		assert self.send_keys_element(*LoginPageLocators.REG_PASSWORD, password), "Reg password is not exist"
+		assert self.send_keys_element(*LoginPageLocators.REG_CPASSWORD, password), "Reg confirm password is not exist"
+		assert self.click_element(*LoginPageLocators.REGISTER_BTN), "Reg btn is not exist"
